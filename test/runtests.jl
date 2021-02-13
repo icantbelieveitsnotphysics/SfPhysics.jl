e4a5bbdd5db054ba7ce26369d56ba400cee0de6b
icantbelieveitsnotphysics <icantbelieveitsnotphysics@runbox.no>
2021-02-13 22:33:47 +0000

    Smoke tests for SfGravity.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,22 @@
 using SfPhysics
 using Test
 
+using Unitful, UnitfulAstro
+import PhysicalConstants.CODATA2018: g_n
+
 @testset "SfPhysics.jl" begin
-    # Write your tests here.
+    @testset "SfGravity" begin
+		# for reasons I'm too lazy to establish, there's a bit of a margin of error here. pretty small though.
+		@test gravity(1u"Mearth", 1u"Rearth") ≈ g_n atol = 0.01u"m/s^2"
+		@test planetary_mass(g_n |> u"m/s^2", 1u"Rearth") ≈ 1u"Mearth" atol=0.01e24u"kg"
+		@test planetary_radius(1u"Mearth", g_n |> u"m/s^2") ≈ 1u"Rearth" atol = 0.01e6u"m"
+		@test orbital_period(1u"Msun", 1.000001018u"AU") ≈ 365.25u"d" atol = 0.01u"d"
+		
+		@test orbital_radius(1u"Msun", 365.25u"d") ≈ 1.000001018u"AU" atol = 0.0001u"AU"
+		
+		# close enough.
+		@test orbital_velocity(1.000001018u"AU", 365.25u"d") ≈ 29.78u"km/s" atol = 0.01u"km/s"
+		@test orbital_velocity(1.000001018u"AU", 365.25u"d", 0.0017) ≈ 29.78u"km/s" atol = 0.01u"km/s"		
+		@test escape_velocity(1u"Mearth", 1u"Rearth") ≈ 11.186u"km/s" atol = 0.01u"km/s"
+	end
 end
