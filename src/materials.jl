eb223a3f9ef930eb9d18d34308ac2a2d37f58584
icantbelieveitsnotphysics <icantbelieveitsnotphysics@runbox.no>
2021-02-22 18:42:05 +0000

    Fix a compile issue, start adding some gas related stuff (used by jeans code in planetary module)

--- src/materials.jl
+++ src/materials.jl
@@ -1,11 +1,15 @@
 using Unitful
 
-export Material, density, yield_strength
+import PhysicalConstants.CODATA2018: k_B
+
+export Material, density, yield_strength, maxwell_boltzmann_peak_speed
 
 struct Material
 	density::Unitful.Density
-	yield_strength::Unitful.Pressure
+	yield_strength::Union{Nothing, Unitful.Pressure}
 end
+
+Material(density::Unitful.Density) = Material(density, nothing)
 
 aluminium = Material(2.7u"g/cm^3", 270u"MPa") # yield strength for 6061 alloy, https://en.wikipedia.org/wiki/6061_aluminium_alloy
 iron = Material(7.874u"g/cm^3")
@@ -18,3 +22,10 @@ ice = Material(0.9168u"g/cm^3", 0u"Pa")
 
 density(m::Material) = m.density
 yield_strength(m::Material) = m.yield_strength
+
+"""
+    maxwell_boltzmann_peak_speed(m::Unitful.Mass, T::Unitful.Temperature)
+	
+Compute the velocity at the peak of the Maxwell-Boltzmann speed distribution for a gas with molecular mass `m` and temperature `T`.
+"""
+maxwell_boltzmann_peak_speed(m::Unitful.Mass, T::Unitful.Temperature) = sqrt((2k_B*T)/m) |> u"m/s"
