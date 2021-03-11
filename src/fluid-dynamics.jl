069c7fc8fec8c03fd4ed933955122a76fb5b98f1
icantbelieveitsnotphysics <icantbelieveitsnotphysics@runbox.no>
2021-03-11 12:38:40 +0000

    The beginnings of some useful fluid dynamics stuff.

--- src/fluid-dynamics.jl
+++ src/fluid-dynamics.jl
@@ -8,7 +8,7 @@ import ..SfPhysics: Angle, to_angle
 import ..SfPhysics: projectile_displacement, projectile_velocity, projectile_flight_time, 
 	projectile_peak_displacement, projectile_range, projectile_angle
 
-export dynamic_viscosity, drag_force
+export dynamic_viscosity, drag_force, reynolds_number
 
 const Viscosity{T} = Unitful.AbstractQuantity{T, Unitful.𝐌 * Unitful.𝐋^-1 *  Unitful.𝐓^-1, typeof(u"Pa*s")}
 
@@ -18,4 +18,6 @@ dynamic_viscosity(drag_coefficient::Real, ρ_fluid::Unitful.Density, projectile_
 drag_force(drag_coefficient::Real, ρ_fluid::Unitful.Density, reference_area::Unitful.Area, v::Unitful.Velocity) = 
 	0.5ρ_fluid * v^2 * drag_coefficient * reference_area
 
+reynolds_number(v::Unitful.Velocity, ρ_fluid::Unitful.Density, l::Unitful.Length, μ::Viscosity) = (v * ρ_fluid * l) / μ |>u"s/s"
+	
 end
--- src/objects.jl
+++ src/objects.jl
@@ -7,5 +7,6 @@ import ..SfMatter: mass, density
 export mass
 
 mass(m::Material, s::Shape) = density(m) * volume(s) |> u"kg"
+mass(d::Unitful.Density, s::Shape) = d * volume(s) |> u"kg"
 
 end
