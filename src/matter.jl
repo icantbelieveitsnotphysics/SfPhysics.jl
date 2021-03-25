9cb3bd4fb5376fb321cb5e33ded8bfbc151406c9
icantbelieveitsnotphysics <icantbelieveitsnotphysics@runbox.no>
2021-03-25 22:38:29 +0000

    Add some bouyancy methods.

--- src/matter.jl
+++ src/matter.jl
@@ -2,10 +2,10 @@ module SfMatter
 
 using Unitful
 
-import PhysicalConstants.CODATA2018: k_B
+import PhysicalConstants.CODATA2018: k_B, g_n
 import ..SfGeometry: volume
 
-export Material, density, yield_strength, maxwell_boltzmann_peak_speed
+export Material, density, yield_strength, maxwell_boltzmann_peak_speed, bouyancy
 
 struct Material
 	density::Unitful.Density
@@ -28,5 +28,33 @@ yield_strength(m::Material) = m.yield_strength
 Compute the velocity at the peak of the Maxwell-Boltzmann speed distribution for a gas with molecular mass `m` and temperature `T`.
 """
 maxwell_boltzmann_peak_speed(m::Unitful.Mass, T::Unitful.Temperature) = sqrt((2k_B*T)/m) |> u"m/s"
+
+"""
+	bouyancy(ρ_body::Unitful.Density, ρ_fluid::Unitful.Density; g = g_n)
+	
+Acceleration due to bouyancy effects experienced by an object with density `ρ_body` in a fluid with density `ρ_fluid`, undergoing acceleration `g` (which defaults to Earth gravity).
+"""
+bouyancy(ρ_body::Unitful.Density, ρ_fluid::Unitful.Density; g = g_n) = upreferred(g * ρ_fluid / ρ_body)
+
+"""
+	bouyancy(ρ_body::Unitful.Density, ρ_fluid::Unitful.Density; g = g_n)
+	
+Acceleration due to bouyancy effects experienced by an object made of `body` in a fluid composed of `fluid`, undergoing acceleration `g` (which defaults to Earth gravity).
+"""
+bouyancy(body::Material, fluid::Material; g = g_n) = upreferred(g * density(fluid) / density(body))
+
+"""
+    bouyancy(v_body::Unitful.Volume, ρ_fluid::Unitful.Density; g = g_n)
+	
+Bouyancy force felt by an object of volume `v_body` in a fluid of density `ρ_fluid`, undergoing acceleration `g` (which defaults to Earth gravity).
+"""
+bouyancy(v_body::Unitful.Volume, ρ_fluid::Unitful.Density; g = g_n) = upreferred(g * ρ_fluid * v_body)
+
+"""
+    bouyancy(v_body::Unitful.Volume, ρ_fluid::Unitful.Density; g = g_n)
+	
+Bouyancy force felt by an object of volume `v_body` in a fluid composed of `fluid`, undergoing acceleration `g` (which defaults to Earth gravity).
+"""
+bouyancy(v_body::Unitful.Volume, fluid::Material; g = g_n) = upreferred(g * density(fluid) * v_body)
 
 end
--- src/objects.jl
+++ src/objects.jl
@@ -2,7 +2,7 @@ module SfObjects
 
 using Unitful, ..SfGeometry, ..SfMatter
 
-import ..SfMatter: mass, density
+import ..SfMatter: mass, density, bouyancy
 
 export mass
 
@@ -19,5 +19,19 @@ mass(m::Material, s::Shape) = density(m) * volume(s) |> u"kg"
 Calculate the mass of an object with shape `s` made of a material with density `d`.
 """
 mass(d::Unitful.Density, s::Shape) = d * volume(s) |> u"kg"
+
+"""
+    bouyancy(s::Shape, ρ_fluid::Unitful.Density; g = g_n)
+	
+Bouyancy force felt by an object of shape `s` in a fluid of density `ρ_fluid`, undergoing acceleration `g` (which defaults to Earth gravity).
+"""
+bouyancy(s::Shape, ρ_fluid::Unitful.Density; g = g_n) = bouyancy(volume(s), ρ_fluid, g)
+
+"""
+    bouyancy(s::Shape, ρ_fluid::Unitful.Density; g = g_n)
+	
+Bouyancy force felt by an object of shape `s` in a medium made of `fluid`, undergoing acceleration `g` (which defaults to Earth gravity).
+"""
+bouyancy(s::Shape, fluid::Material; g = g_n) = bouyancy(volume(s), density(fluid), g)
 
 end
