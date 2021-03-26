1727eb4dfaf8e89fdde40625e3f2b17d5398743e
icantbelieveitsnotphysics <icantbelieveitsnotphysics@runbox.no>
2021-03-26 19:45:46 +0000

    Add some docs, fix some names, use abstract quantities for despatch to support PhysicalConstants.

--- src/rocketry.jl
+++ src/rocketry.jl
@@ -3,21 +3,49 @@ module SfRocketry
 import PhysicalConstants.CODATA2018: c_0, g_n
 using Unitful
 
-import ..SfUnits: Angle, to_angle
+import ..SfUnits: Angle, to_angle, Acceleration, Speed
 
 export rocket_propulsive_efficiency, brachistochrone_transit_time, brachistochrone_acceleration, brachistochrone_delta_v,
-	boost_coast_transit_time, coasting_coast_time, beam, mass_ratio, delta_v, rocket_thrust, rocket_power,
+	boost_coast_transit_time, boost_coast_thrust_time, beam, mass_ratio, delta_v, rocket_thrust, rocket_power,
 	mass_flow, exhaust_velocity, shkadov_thrust
 
-rocket_propulsive_efficiency(ship_velocity::Unitful.Velocity, exhaust_velocity::Unitful.Velocity) = 2(ship_velocity / exhaust_velocity) / (1+(ship_velocity / exhaust_velocity)^2)
+rocket_propulsive_efficiency(ship_velocity::Speed, exhaust_velocity::Speed) = 
+	2(ship_velocity / exhaust_velocity) / (1+(ship_velocity / exhaust_velocity)^2)
 
-brachistochrone_transit_time(d::Unitful.Length, a::Unitful.Acceleration) = 2sqrt(d/a)
-brachistochrone_acceleration(d::Unitful.Length, t::Unitful.Time) = 4d/t^2
-brachistochrone_delta_v(d::Unitful.Length, a::Unitful.Acceleration) = 2sqrt(d*a)
+"""
+    brachistochrone_transit_time(d::Unitful.Length, a::Acceleration)
+	
+Time to travel a distance of `d` given a constant acceleration of `a` and a final velocity of 0 relative to the start.
+"""
+brachistochrone_transit_time(d::Unitful.Length, a::Acceleration) = upreferred(2sqrt(d/a))
 
-boost_coast_transit_time(d::Unitful.Length, a::Unitful.Acceleration, boost::Unitful.Time) = ((d - (a * boost^2)) / (a * boost)) + 2boost
+"""
+	brachistochrone_acceleration(d::Unitful.Length, t::Unitful.Time)
+	
+Required constant acceleration to travel a distance of `d` in time `t` with a final velocity of 0 relative to the start.
+"""
+brachistochrone_acceleration(d::Unitful.Length, t::Unitful.Time) = upreferred(4d/t^2)
 
-function coasting_coast_time(dist::Unitful.Length, acc::Unitful.Acceleration, transit::Unitful.Time)
+"""
+	brachistochrone_delta_v(d::Unitful.Length, a::Acceleration)
+	
+Required ΔV to travel a distance `d` with constant acceleration `d` and attain a final velocity of 0 relative to the start.
+"""
+brachistochrone_delta_v(d::Unitful.Length, a::Acceleration) = 2sqrt(d*a) |> u"km/s"
+
+"""
+    boost_coast_transit_time(d::Unitful.Length, a::Acceleration, boost::Unitful.Time)
+	
+Total time to travel a distance of `d`, given speedup and slowdown phases of length `boost` undergoing acceleration `a`.
+"""
+boost_coast_transit_time(d::Unitful.Length, a::Acceleration, boost::Unitful.Time) = ((d - (a * boost^2)) / (a * boost)) + 2boost
+
+"""
+    boost_coast_thrust_time(dist::Unitful.Length, acc::Acceleration, transit::Unitful.Time)
+	
+Combined duration of boost and brake phases required to cross a distance of `dist` in time `transit` given an acceleration under thrust of `acc`.
+"""
+function boost_coast_thrust_time(dist::Unitful.Length, acc::Acceleration, transit::Unitful.Time)
 	# Tt - t^2 - d/a
 	# ax^2 + bx + c
 
@@ -39,7 +67,7 @@ function coasting_coast_time(dist::Unitful.Length, acc::Unitful.Acceleration, tr
 	end
 end
 
-function beam_core_mass_ratio(a, dv::Unitful.Velocity, ve::Unitful.Velocity)
+function beam_core_mass_ratio(a, dv::Speed, ve::Speed)
 	k1 = sqrt((1-a)^2+4a*ve^2/c_0^2)
 	k2 = (-2ve * dv / c_0^2) + 1 - a
 
@@ -53,22 +81,22 @@ function beam_core_mass_ratio(a, dv::Unitful.Velocity, ve::Unitful.Velocity)
 end
 
 """
-    mass_ratio(dv::Unitful.Velocity, ve::Unitful.Velocity)
+    mass_ratio(dv::Speed, ve::Speed)
 	
 Compute the mass ratio for a rocket with total delta-V `dv` and exhaust velocity `ve`.
 """
-mass_ratio(dv::Unitful.Velocity, ve::Unitful.Velocity) = exp(dv/ve)
+mass_ratio(dv::Speed, ve::Speed) = exp(dv/ve)
 
-delta_v(ve::Unitful.Velocity, mr::Real) = ve * log(mr)
+delta_v(ve::Speed, mr::Real) = ve * log(mr)
 delta_v(isp::Unitful.Time, mr::Real) = isp * g_n * log(mr)
 
-rocket_thrust(ve::Unitful.Velocity, mdot::Unitful.MassFlow) = Unitful.uconvert(Unitful.N, mdot * ve)
-rocket_thrust(ve::Unitful.Velocity, fp::Unitful.Power) = Unitful.uconvert(Unitful.N, (2 * fp) / ve)
+rocket_thrust(ve::Speed, mdot::Unitful.MassFlow) = Unitful.uconvert(Unitful.N, mdot * ve)
+rocket_thrust(ve::Speed, fp::Unitful.Power) = Unitful.uconvert(Unitful.N, (2 * fp) / ve)
 
-rocket_power(ve::Unitful.Velocity, mdot::Unitful.MassFlow) = Unitful.uconvert(Unitful.W, (mdot * ve^2) / 2)
-rocket_power(ve::Unitful.Velocity, thrust::Unitful.Force) = Unitful.uconvert(Unitful.W, (thrust * ve) / 2)
+rocket_power(ve::Speed, mdot::Unitful.MassFlow) = Unitful.uconvert(Unitful.W, (mdot * ve^2) / 2)
+rocket_power(ve::Speed, thrust::Unitful.Force) = Unitful.uconvert(Unitful.W, (thrust * ve) / 2)
 
-mass_flow(ve::Unitful.Velocity, thrust::Unitful.Force) = Unitful.uconvert(u"kg/s", thrust / ve)
+mass_flow(ve::Speed, thrust::Unitful.Force) = Unitful.uconvert(u"kg/s", thrust / ve)
 
 """
     exhaust_velocity(isp::Unitful.Time)
